e1c4edc683930732ef1cac36e32efc8cff025e41
Bruno Cabral <bruno.cabral88@hotmail.com>
2017-04-02 20:17:43 +0000

    Initial twilio implementation

--- routes/orders.js
+++ routes/orders.js
@@ -3,6 +3,17 @@ var router = express.Router();
 const dbsettings = require('../lib/DbSettings')
 const knex = require('knex')(dbsettings);
 const DataHelpers = require('../lib/DataHelpers')(knex);
+const twilio = require('twilio');
+const dotnev = require('dotenv').config({path:'../'});
+const client = twilio(process.env.TWILIO_ACCOUNT_SID,process.env.TWILIO_TOKEN);
+
+function sendSMS(cust_number,body,callback) {
+	client.sendMessage({
+		body: body,
+		to: cust_number,
+		from: '+13065000498'
+	},callback);
+}
 
 // Post to a new order
 router.post('/',(req,res) => {
@@ -11,6 +22,12 @@ router.post('/',(req,res) => {
 	DataHelpers.insertOrder(JSON.parse(req.body.items),null,'807-213-2133',function(order_id){
 		console.log('Order id:',order_id);
 	});
+});
+
+router.get('/sms',(req,res) => {
+	sendSMS('+14388302735','Testing again from Plentyful',(err,result) => {
+		res.send(result);
+	})
 })
 
 module.exports = router;
